ed8ee013ddcf8a3ea78a1c6f8cdd50fa08d5a43a
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-25 11:43:53 +0700

    Mengurutkan rekaman-rekaman dalam urutan menurun berdasarkan nomor roll.

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -21,7 +21,7 @@ public:
 	bool deleteNode(int rollNo);
 	bool listEmpty();
 	void ascending();
-	void descanding();
+	void descending();
 	void hapus();
 	void searchData();
 };
@@ -110,6 +110,22 @@ void DoubleLinkedList::ascending() {
 		while (currentNode != NULL) {
 			cout << currentNode->noMhs << " " << currentNode->name << endl;
 			currentNode = currentNode->next;
+		}
+	}
+}
+
+void DoubleLinkedList::descending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in descending order of role number are" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
 		}
 	}
 }
