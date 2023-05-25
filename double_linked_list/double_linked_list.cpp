210c68704ccdd7d05abfdf7709dcfbee443d472a
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-25 12:18:39 +0700

    Menjalankan program utama dengan menggunakan objek dari kelas DoubleLinkedList.

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -178,8 +178,31 @@ int main() {
 			char ch;
 			cin >> ch;
 
-
-
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.ascending();
+				break;
+			case '4':
+				obj.descending();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
 }
